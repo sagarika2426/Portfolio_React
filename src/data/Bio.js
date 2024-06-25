a440aba541c8d8de4c17b37369c245af68ea760a
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2024-06-25 16:40:00 +0530

    resume link change

--- src/data/Bio.js
+++ src/data/Bio.js
@@ -6,7 +6,7 @@ export const Bio = {
         "Programmer. ",
     ],
     description: "As a Frontend Developer, I focus on creating seamless, user-friendly interfaces with a variety of modern web technologies. My passion lies in translating design concepts into functional, high-quality code, ensuring a smooth and enjoyable user experience on every project",
-    resume: "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    resume: "https://drive.google.com/file/d/1n76Lzvc4s_xOrQvEBsJL0SgcNLM4bvKq/view?usp=sharing",
     github: "https://github.com/sagarika2426",
     linkedin: "https://www.linkedin.com/in/sagarika2412/",
     x : "https://x.com/_Sagarika_Sahoo"};

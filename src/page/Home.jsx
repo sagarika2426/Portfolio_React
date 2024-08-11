f61debd28c841095487a5652acd0060dd4632059
Sagarika Sahoo <sagarikasahoo16@gmail.com>
2024-08-11 14:43:23 +0530

    removed exp

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,16 +1,13 @@
-import React from 'react'
 import About from '../components/About'
 import Skills from '../components/Skills'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
-import Experience from '../components/Experience'
 
 function Home() {
   return (
     <>
       <About />
       <Skills />
-      {/* <Experience /> */}
       <Projects />
       <Contact />
     </>
